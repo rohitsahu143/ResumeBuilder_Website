87fc736976778730abca56c048683e47237a488e
Rohit Sahu <sahuankit89011@gmail.com>
2026-09-20 12:35:16 +0530

    Configure database for cloud deployment

--- src/main/java/com/resumebuilder/util/DBConnection.java
+++ src/main/java/com/resumebuilder/util/DBConnection.java
@@ -6,10 +6,6 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/resume_builder";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root"; 
-
     public static Connection getConnection() {
 
         Connection con = null;
@@ -17,15 +13,26 @@ public class DBConnection {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            con = DriverManager.getConnection(URL, USER, PASSWORD);
+            String host = System.getenv("MYSQLHOST");
+            String port = System.getenv("MYSQLPORT");
+            String database = System.getenv("MYSQLDATABASE");
+            String user = System.getenv("MYSQLUSER");
+            String password = System.getenv("MYSQLPASSWORD");
+
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database
+                    + "?useSSL=false&serverTimezone=UTC";
+
+            con = DriverManager.getConnection(url, user, password);
 
             System.out.println("Database Connected Successfully!");
 
         } catch (ClassNotFoundException e) {
+
             System.out.println("MySQL Driver Not Found!");
             e.printStackTrace();
 
         } catch (SQLException e) {
+
             System.out.println("Connection Failed!");
             e.printStackTrace();
         }
